4cd00b6cb6843fa24d59cc83cbe5ad2fb018eda2
Christine Anne Valenciano <111478476+tanvalencianox@users.noreply.github.com>
2022-09-05 10:16:01 +0800

    Add files via upload

---
+++ exam_1/Student.java
@@ -0,0 +1,45 @@
+public class Student {
+    // Properties
+    
+    // firstname
+    String firstname;
+
+    String middlename;
+
+    String surname;
+
+    String middleInitial;
+
+    String dateOfBirth;
+
+    int studentNumber;
+
+    String studentEmailAddress;
+
+    boolean iAmAwesome;
+
+    public void sayMyStudentNumber() {
+
+        System.out.println("Student Number:"+studentNumber);
+
+    }
+
+    public void sayMyEmailAddress() {
+
+        System.out.println("Email Address:"+studentEmailAddress);
+
+    }
+
+    public void iAmAwesome() {
+
+        System.out.println("i Am Awesome= "+ iAmAwesome +"\n");
+
+    }
+
+
+
+
+}
+
+
+
