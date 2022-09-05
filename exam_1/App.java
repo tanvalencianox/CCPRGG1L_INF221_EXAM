508fe02c100538ba20b66cb0b3dc3b7e107da2b7
Christine Anne Valenciano <111478476+tanvalencianox@users.noreply.github.com>
2022-09-05 10:15:35 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+        // Student class
+        Student Me = new Student ();
+         
+        Me.firstname = "christine";
+        Me.surname = "valenciano";
+        Me.middlename = "C";
+        Me.studentNumber = 2022109128;
+        Me.studentEmailAddress = "valencianocc@students.national-u.edu.ph";
+        Me.iAmAwesome = true;
+        Me.sayMyStudentNumber();
+        Me.sayMyEmailAddress();
+        Me.iAmAwesome();
+    }
+}   
